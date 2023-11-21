50d66a17d19d274717850b8f91667696b49a616e
Amadeus <williancoden@gmail.com>
2023-11-21 01:05:43 -0300

    Feat: findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.Date;
+import java.util.List;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -18,6 +19,15 @@ public class Program {
 	Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
+		
+		
+		System.out.println("\n=== TEST 2: seller findByDepartment	=====");
+		Department department = new Department(2,null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		
+	    for(Seller obj : list) {
+	    	System.out.println(obj);
+	    }
 	}
 
 }
