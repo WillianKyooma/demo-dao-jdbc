f978648f60a6938f2e51ad648d28d325fd76501f
Amadeus <williancoden@gmail.com>
2023-11-22 01:02:52 -0300

    Feat: findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -28,6 +28,16 @@ public class Program {
 	    for(Seller obj : list) {
 	    	System.out.println(obj);
 	    }
+	    
+	    System.out.println("\n=== TEST 3: seller findAll	=====");
+		
+		 list = sellerDao.findAll();
+		
+	    for(Seller obj : list) {
+	    	System.out.println(obj);
+	    }
+	    
+	    
 	}
 
 }
