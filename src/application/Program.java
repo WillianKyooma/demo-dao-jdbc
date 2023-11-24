f1836c3070336aefb96f8e33f752ec3bc075e122
Amadeus <williancoden@gmail.com>
2023-11-24 11:54:33 -0300

    Feat:update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,10 +38,18 @@ public class Program {
 	    }
 	    
 	    
-	    System.out.println("\n=== TEST 2: seller findByDepartment	=====");
+	    System.out.println("\n=== TEST 4: seller Insert	=====");
 	    Seller newSeller = new Seller(null, "Greg", "Greg@gmail.com",new Date(),4000.0,department);
 	    sellerDao.insert(newSeller);
 	    System.out.println("Inserted new Id = " + newSeller.getId());
+	    
+	    System.out.println("\n=== TEST 5: seller SellerUpdate	=====");
+	    
+	    seller = sellerDao.findById(1);
+	    seller.setName("Martha Waine");
+	    sellerDao.update(seller);
+	    System.out.println("Update completed");
+	    
 	}
 
 }
